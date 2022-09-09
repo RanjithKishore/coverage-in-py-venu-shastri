a0896c9263de5dae2a532f5ad8d153472299bba6
Ranjith Kishore A V <avrk2000@gmail.com>
2022-09-09 15:41:00 +0530

    Update string_calculator.test.py

--- string_calculator.test.py
+++ string_calculator.test.py
@@ -1,1 +1,7 @@
+import unittest
+import string_calculator
 
+
+class CalculatorTest(unittest.TestCase):
+  def givenEmptyStringZeroIsExpected(self):
+    self.assertTrue(string_calculator.add("") == 0)
