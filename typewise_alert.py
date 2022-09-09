ddc653def512612bb9efaaec9b6e3a1059152da3
Ranjith Kishore A V <avrk2000@gmail.com>
2022-09-09 14:16:36 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -12,6 +12,7 @@ def classify_temperature_breach(coolingType, temperatureInC):
   upperLimit = 0
   if coolingType == 'PASSIVE_COOLING':
     lowerLimit = 0
+    
     upperLimit = 35
   elif coolingType == 'HI_ACTIVE_COOLING':
     lowerLimit = 0
